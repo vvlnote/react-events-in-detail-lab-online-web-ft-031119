d1b387971115e4b842b160cb8b4bfc5a2e3feae0
Joy Lin <vvlnote@hotmail.com>
2019-07-29 03:48:49 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -7,11 +7,9 @@ export default class CoordinatesButton extends Component {
     let coordinates = new Array();
     coordinates[0] = e.clientX;
     coordinates[1] = e.clientY;
-<<<<<<< HEAD
+
     this.props.onReceiveCoordinates(coordinates);
-=======
-    return this.props.onReceiveCoordinates(coordinates);
->>>>>>> 5c9635aa91be4c6e9c24e96b9d48780f14859090
+
   }
     render() {
       return (
